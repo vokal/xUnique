440431b0d6a7c8edfa72644b08416c645efc15ce
Ellen Shapiro <designatednerd@gmail.com>
2016-02-05 15:34:22 -0600

    use pods root directly instead of hard-coding

--- xUnique_Run_Script.sh
+++ xUnique_Run_Script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get the path to the local copy of xUnique
-XUNIQUE_PATH="${SRCROOT}/Pods/xUnique/xUnique.py"
+XUNIQUE_PATH="${PODS_ROOT}/xUnique/xUnique.py"
 
 # Run it against the main project
 python "${XUNIQUE_PATH}" "${PROJECT_FILE_PATH}/project.pbxproj"
